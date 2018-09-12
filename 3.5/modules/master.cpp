447d6342612f81c977399aff525806674fb5d20f
GuidoBallabio <guidodino.ballabio@mail.polimi.it>
2018-09-12 21:20:31 +0200

    urgent fix removed randomness

--- 3.5/modules/master.cpp
+++ 3.5/modules/master.cpp
@@ -1,5 +1,4 @@
 #include "master.h"
-#include "../util/tools.h"
 
 void Master::generate()
 {
@@ -8,7 +7,6 @@ void Master::generate()
         int i = 0;
         while (stimulus_in)
         {
-            wait(CLK_PERIOD * (random_int() % 5), SC_NS); //max wait = 5 clk
             data->write(i++);
         }
         wait(CLK_PERIOD, SC_NS);
